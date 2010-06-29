481c360d199448327c77c4ccae4229351a9bebe5
Clément Stenac <clement.stenac@exalead.com>
2010-06-29 13:45:37 +0200

    Add sessions view and integrate metrics details

--- metrolix/server/views.py
+++ metrolix/server/views.py
@@ -92,56 +92,8 @@ def report_result(request):
     return HttpResponse("OK")
 
 ####################################################################
-# Frontend
+# Frontend API
 ####################################################################
-
-def put_projects(template_dict):
-  projects = Project.objects.all()
-  template_dict["projects"] = []
-  for project in projects:
-    template_dict["projects"].append(project.name)
-
-
-def index(request):
-    ret = {}
-    put_projects(ret)
-    return render_to_response("index.html", ret)
-
-def metric_details(request, project, path):
-    try:
-        proj_obj = Project.objects.get(name=project)
-    except:
-        return HttpResponseNotFound("Project not found")
-    try:
-        metric_obj = Metric.objects.get(path=path, project=proj_obj)
-    except:
-        return HttpResponseNotFound("Metric not found")
-
-    return render_to_response("metric_details.html",
-            {"metric": metric_obj, "results" : metric_obj.result_set.all()})
-
-    return HttpResponse("project=%s path=%s" % (project, path))
-
-def metrics_view_noproject(request):
-    if not "project_name" in request.session:
-        return HttpResponseNotFound("No project currently active")
-    return metrics_view(request, request.session["project_name"])
-
-def metrics_view(request, project):
-    try:
-        proj_obj = Project.objects.get(name=project)
-    except:
-        return HttpResponseNotFound("Project not found")
-
-    metrics = Metric.objects.filter(project=proj_obj)
-
-    for metric in metrics:
-        metric.nb_values = len(metric.result_set.all())
-        metric.last_value = 0
-
-    ret =  {"project": proj_obj, "metrics" : metrics}
-    put_projects(ret)
-    return render_to_response("metrics_view.html", ret)
 
 def json_metrics_list(request, project):
     try:
@@ -149,15 +101,9 @@ def json_metrics_list(request, project):
     except:
         return HttpResponseNotFound("Project not found")
 
-    try:
-      data = request.GET#simplejson.loads(request.raw_post_data)
-    except:
-      return HttpResponseBadRequest("Could not deserialize JSON request")
-
     metrics = Metric.objects.filter(project=proj_obj)
-
-    if data.has_key("path_filter"):
-        metrics = metrics.filter(path__contains=data["path_filter"]).order_by("path")
+    if request.GET.has_key("path_filter"):
+        metrics = metrics.filter(path__contains=request.GET["path_filter"]).order_by("path")
 
     ret = []
     for metric in metrics:
@@ -165,9 +111,56 @@ def json_metrics_list(request, project):
                     "nb_values" : len(metric.result_set.all())})
     return HttpResponse(simplejson.dumps(ret))
 
+def json_sessions_list(request, project):
+    try:
+        proj_obj = Project.objects.get(name=project)
+    except:
+        return HttpResponseNotFound("Project not found")
+
+    sessions = Session.objects.filter(project=proj_obj)
+
+    if request.GET.has_key("host"):
+        sessions = sessions.filter(host__name__contains=request.GET["host"])
+    if request.GET.has_key("os"):
+        sessions = sessions.filter(host__os__contains=request.GET["os"])
+    if request.GET.has_key("architecture"):
+        sessions = sessions.filter(host__architecture__contains=request.GET["architecture"])
+
+    sessions = sessions.order_by("-date")
+
+    ret = []
+    for session in sessions:
+        rs = {"date" : time.mktime(session.date.timetuple())}
+        rs["token"] = session.token
+        rs["sessionid"] = session.id
+        if session.host is not None:
+            rs["os"] = session.host.os
+            rs["architecture"] = session.host.architecture
+            rs["hostname"] = session.host.name
+            rs["hostid"] = session.host.id
+        ret.append(rs)
+    return HttpResponse(simplejson.dumps(ret))
+
+def json_session_results(request, project):
+    try:
+        proj_obj = Project.objects.get(name=project)
+    except:
+        return HttpResponseNotFound("Project not found")
+
+    results = Result.objects.filter(session__token=request.REQUEST["token"])
+    results.order_by("metric__path")
+    ret = []
+    for result in results:
+        rs = { "path": result.metric.path, "title": result.metric.title,
+              "type": result.metric.type, "value": result.value}
+        ret.append(rs)
+    return HttpResponse(simplejson.dumps(ret))
+
+
 def json_set_project(request):
     request.session["project_name"] = request.REQUEST["project_name"]
     return HttpResponse("OK")
+
 
 def json_metrics_data(request, project):
     try:
@@ -181,7 +174,6 @@ def json_metrics_data(request, project):
       return HttpResponseBadRequest("Could not deserialize JSON request")
 
     ret = {}
-
 
     metrics = Metric.objects.filter(project=proj_obj, path__in=data.getlist("path"))
     path_to_idx = {}
@@ -204,17 +196,88 @@ def json_metrics_data(request, project):
 
     ret["sessions"] = sessions.values()
     ret["sessions"] = sorted(ret["sessions"], key = lambda x : x["date"])
-#   for metric in metrics:
-#        out_vals = []
-#        results = Result.objects.filter(metric=metric)
-#        if data.has_key("architecture"):
-#            results = results.filter(session__host__architecture=data["architecture"])
-#        #if data.has_key("host_name"):
-#        #    metrics = metrics.filter(session__host__name=data["host_name"])
-#        for result in results:
-#            out_vals.append({"value": result.value, "date": time.mktime(result.session.date.timetuple())})
-#
-#        ret.append({"metric": {"path": metric.path, "title": metric.title},
-#                    "values": out_vals})
-
     return HttpResponse(simplejson.dumps(ret))
+
+def json_delete_result(request):
+    id = request.REQUEST["id"]
+    Result.objects.get(pk=id).delete()
+    return HttpResponse("ok")
+
+####################################################################
+# Frontend pages
+####################################################################
+
+def put_projects(request, template_dict):
+  if not "project_name" in request.session:
+      request.session["project_name"] = Project.objects.all()[0].name
+
+  projects = Project.objects.all()
+  template_dict["projects"] = []
+  cur = None
+  if "project_name" in request.session:
+      cur = request.session["project_name"]
+  for project in projects:
+    if cur is not None and project.name == cur:
+        old = template_dict["projects"]
+        template_dict["projects"] = [project.name]
+        template_dict["projects"].extend(old)
+    else:
+        template_dict["projects"].append(project.name)
+
+def index(request):
+    ret = {}
+    put_projects(request, ret)
+    return render_to_response("index.html", ret)
+
+def metric_details_raw(request):
+    return metric_details(request, request.session["project_name"], request.REQUEST["path"])
+
+def metric_details(request, project, path):
+    try:
+        proj_obj = Project.objects.get(name=project)
+    except:
+        return HttpResponseNotFound("Project not found")
+    try:
+        metric_obj = Metric.objects.get(path=path, project=proj_obj)
+    except:
+        return HttpResponseNotFound("Metric not found")
+
+    ret =  {"metric": metric_obj, "results" : metric_obj.result_set.all().order_by("session__date")}
+    put_projects(request, ret)
+    return render_to_response("metric_details.html", ret)
+
+def metrics_view_noproject(request):
+    if not "project_name" in request.session:
+        return HttpResponseNotFound("No project currently active")
+    return metrics_view(request, request.session["project_name"])
+
+def metrics_view(request, project):
+    try:
+        proj_obj = Project.objects.get(name=project)
+    except:
+        return HttpResponseNotFound("Project not found")
+
+    metrics = Metric.objects.filter(project=proj_obj)
+
+    for metric in metrics:
+        metric.nb_values = len(metric.result_set.all())
+        metric.last_value = 0
+
+    ret =  {"project": proj_obj, "metrics" : metrics}
+    put_projects(request, ret)
+    return render_to_response("metrics_view.html", ret)
+
+def sessions_view_noproject(request):
+    if not "project_name" in request.session:
+        return HttpResponseNotFound("No project currently active")
+    return sessions_view(request, request.session["project_name"])
+
+def sessions_view(request, project):
+    try:
+        proj_obj = Project.objects.get(name=project)
+    except:
+        return HttpResponseNotFound("Project not found")
+
+    ret =  {"project": proj_obj}
+    put_projects(request, ret)
+    return render_to_response("sessions_view.html", ret)
--- metrolix/urls.py
+++ metrolix/urls.py
@@ -1,31 +1,40 @@
 from django.conf.urls.defaults import *
 from metrolix import settings
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
+    #################### Backend Reporting server #################
     (r'^server/start_session', "metrolix.server.views.start_session"),
     (r'^server/report_result', "metrolix.server.views.report_result"),
 
+    #################### Frontend API #################
+    (r'^json_api/metrics_list/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.json_metrics_list"),
+
+    (r'^json_api/sessions_list/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.json_sessions_list"),
+    (r'^json_api/session_results/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.json_session_results"),
+
+    (r'^json_api/set_project', "metrolix.server.views.json_set_project"),
+    (r'^json_api/metrics_data/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.json_metrics_data"),
+    (r'^json_api/delete_result', "metrolix.server.views.json_delete_result"),
+
+
+    #################### Frontend pages #################
     (r'^dashboard/metric/(?P<project>[\.A-z0-9_-]+)/(?P<path>.*)$', "metrolix.server.views.metric_details"),
+    (r'^dashboard/metric_details$', "metrolix.server.views.metric_details_raw"),
+
     (r'^dashboard/metrics_view$', "metrolix.server.views.metrics_view_noproject"),
     (r'^dashboard/metrics_view/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.metrics_view"),
 
+    (r'^dashboard/sessions_view$', "metrolix.server.views.sessions_view_noproject"),
+    (r'^dashboard/sessions_view/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.sessions_view"),
+
+    # Catch-all
     (r'^dashboard', "metrolix.server.views.index"),
 
-    (r'^json_api/metrics_list/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.json_metrics_list"),
-    (r'^json_api/set_project', "metrolix.server.views.json_set_project"),
-    (r'^json_api/metrics_data/(?P<project>[\.A-z0-9_-]+)$', "metrolix.server.views.json_metrics_data"),
-
-    # Common media                                                                                     
+    # Common media
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {"document_root": settings.MEDIA_ROOT}),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    ################### Admininistration frontend ################
     (r'^admin/', include(admin.site.urls)),
 )
