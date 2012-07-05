84257c72be360ea2a3f3f6325976ab6d7569428c
Clément Stenac <clement.stenac@exalead.com>
2012-07-05 17:17:35 +0200

    Java client now handles all session parameters

--- client/java/src/com/exalead/metrolix/MetrolixClient.java
+++ client/java/src/com/exalead/metrolix/MetrolixClient.java
@@ -10,14 +10,25 @@ import java.net.URL;
 public class MetrolixClient {
     /**
      * Set parameters to report to metrolix server.
-     * If parameters are not set, the METROLIX_SERVER and METROLIX_PROJECT
+     * If parameters are not set and not given in the createSession call, the
+     * METROLIX_SERVER, METROLIX_PROJECT, METROLIX_PROJECT_BRANCH, METROLIX_PROJECT_VERSION,
+     * METROLIX_SESSION_NAME and METROLIX_SESSION_TESTSET
      * environment variables are used
      */
-    public static void setParams(String _server, String _project) {
-        server = _server;
-        project = _project;
+    public static void setParams(String server, String project, String projectBranch, String projectVersion,
+            String sessionName, String sessionTestset) {
+        MetrolixClient.server = server;
+        MetrolixClient.project = project;
+        MetrolixClient.projectBranch = projectBranch;
+        MetrolixClient.projectVersion = projectVersion;
+        MetrolixClient.sessionName = sessionName;
+        MetrolixClient.testset = sessionTestset;
     }
-    
+
+    protected static boolean enabled() {
+        return server != null || System.getenv("METROLIX_SERVER") != null; 
+    }
+
     /**
      * Get an object to report to the Metrolix server using an already-started
      * session.
@@ -25,20 +36,15 @@ public class MetrolixClient {
      * METROLIX_SESSION environment variable
      */
     public synchronized static MetrolixSession retrieveSession() {
+        if (!enabled()) {
+            System.out.println("Metrolix server disabled");
+            return new MetrolixSession();
+        }
         if (savedSession != null) {
             return savedSession;
         }
-        if (server == null || project == null) {
-            server = System.getenv("METROLIX_SERVER");
-            project = System.getenv("METROLIX_PROJECT");
-        }
-        if (server == null || project == null) {
-            enabled = false;
-        }
-        
         MetrolixSession ps = new MetrolixSession();
         if (!enabled) {
-            System.out.println("Metrolix server disabled");
             return ps;
         }
         if (savedSessionToken == null) {
@@ -49,27 +55,42 @@ public class MetrolixClient {
         return ps;
     }
     
-    /**
-     * Create a new reporting session on the Metrolix server.
-     * The session can be reused by calling retrieveSession()
-     */
     public synchronized static MetrolixSession newSession() {
-        if (server == null || project == null) {
-            server = System.getenv("METROLIX_SERVER");
-            project = System.getenv("METROLIX_PROJECT");
-        }
-        if (server == null || project == null) {
-            enabled = false;
-        }
+        return newSession(null, null, null, null, null, null);
+    }
+
+    public synchronized static MetrolixSession newSession(String server, String project,
+            String projectBranch, String projectVersion, String sessionName, String sessionTestset) {
+        if (!enabled()) return new MetrolixSession();
+        return newSessionResolved(
+                server != null ? server : (MetrolixClient.server != null ? MetrolixClient.server : System.getenv("METROLIX_SERVER")),
+                        project != null ? project : (MetrolixClient.project != null ? MetrolixClient.project : System.getenv("METROLIX_PROJECT")),
+                                projectBranch != null ? projectBranch : (MetrolixClient.projectBranch != null ? MetrolixClient.projectBranch : System.getenv("METROLIX_PROJECT_BRANCH")),
+                                        projectVersion != null ? projectVersion : (MetrolixClient.projectVersion != null ? MetrolixClient.projectVersion : System.getenv("METROLIX_PROJECT_VERSION")),
+                                                sessionName != null ? sessionName : (MetrolixClient.sessionName != null ? MetrolixClient.sessionName: System.getenv("METROLIX_SESSION_NAME")),
+                                                        sessionTestset != null ? sessionTestset : (MetrolixClient.testset != null ? MetrolixClient.testset: System.getenv("METROLIX_SESSION_TESTSET")));
+    }
+
+    private static String jsonEscape(String x) {
+        return x.replace("\"", "\\\"");
+    }
+
+    private synchronized static MetrolixSession newSessionResolved(String server, String project,
+            String projectBranch, String projectVersion, String sessionName, String sessionTestset) {
+
+        if (server == null) throw new IllegalArgumentException("Could not find metrolix server");
+        if (project== null) throw new IllegalArgumentException("Could not find metrolix project");
         
         MetrolixSession ps = new MetrolixSession();
-        if (!enabled) {
-            System.out.println("Metrolix server disabled");
-            return ps;
-        }
 
         try {
-            String content = "{\"project_name\":\"" + project + "\"}";
+            String content = "{" +
+                    "\"project_name\":\"" + jsonEscape(project) + "\"," +
+                    (projectVersion != null ? "\"version\":\"" + jsonEscape(projectVersion) + "\","  : "")+ 
+                    (projectBranch != null ? "\"branch\":\"" + jsonEscape(projectBranch) + "\","  : "" )+ 
+                    (sessionName != null ? "\"session_name\":\"" + jsonEscape(sessionName) + "\","  : "") + 
+                    (sessionTestset != null ? "\"testset\":\"" + jsonEscape(sessionTestset) + "\""  : "") + 
+                    "}";
             URL target = new URL(server  + "/server/start_session");
             HttpURLConnection connection = (HttpURLConnection) target.openConnection();
             connection.setDoOutput(true);
@@ -94,6 +115,11 @@ public class MetrolixClient {
 
     static String server;
     static String project;
+    static String projectBranch;
+    static String projectVersion;
+    static String testset;
+    static String sessionName;
+
     static String savedSessionToken;
     static MetrolixSession savedSession;
     static boolean enabled = true;
